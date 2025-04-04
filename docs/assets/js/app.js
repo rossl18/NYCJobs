d03c2cb509b7fe38f5869c570a02d4d866ca3b3c
Ross Lauterbach <38871143+rossl18@users.noreply.github.com>
2025-04-04 13:23:26 -0400

    Update app.js

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('NYCJobs/docs/companies.json')
+  fetch('/NYCJobs/docs/companies.json')
     .then(response => response.json())
     .then(data => {
       const list = document.getElementById('company-list');
